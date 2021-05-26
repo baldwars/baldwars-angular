f0510d050078ae22e61090a0a993bd4931b4a0b2
Antonin Texier <antonin.texier@gmail.com>
2021-05-26 10:18:57 +0200

    config(env): Change api endpoint in environments with https.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  endpoint: 'https://baldwars-api.chukitipok.qtmsheep.com/api',
+  endpoint: 'http://baldwars-api:70000/api',
   production: false
 };
 
