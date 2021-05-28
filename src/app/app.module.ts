d5072fbbc7f1c84f244e0a64a54a08bf87c2ffb2
Antonin Texier <antonin.texier@gmail.com>
2021-05-28 10:41:52 +0200

    config(app): Move notifier module options in a separate file.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,48 +15,8 @@ import { EditorComponent } from './views/editor/editor.component';
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { FlexLayoutModule } from "@angular/flex-layout";
 import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
-import { NotifierModule, NotifierOptions } from "angular-notifier";
-
-const notifierDefaultOptions: NotifierOptions = {
-  position: {
-    horizontal: {
-      position: "right",
-      distance: 12
-    },
-    vertical: {
-      position: "top",
-      distance: 12,
-      gap: 10
-    }
-  },
-  theme: "material",
-  behaviour: {
-    autoHide: 5000,
-    onClick: false,
-    onMouseover: "pauseAutoHide",
-    showDismissButton: true,
-    stacking: 4
-  },
-  animations: {
-    enabled: true,
-    show: {
-      preset: "slide",
-      speed: 300,
-      easing: "ease"
-    },
-    hide: {
-      preset: "fade",
-      speed: 300,
-      easing: "ease",
-      offset: 50
-    },
-    shift: {
-      speed: 300,
-      easing: "ease"
-    },
-    overlap: 150
-  }
-};
+import { NotifierModule } from "angular-notifier";
+import { notifierOptions } from './notifier.config';
 
 @NgModule({
   declarations: [
@@ -78,7 +38,7 @@ const notifierDefaultOptions: NotifierOptions = {
     AngularMaterialModule,
     AppRoutingModule,
     FlexLayoutModule,
-    NotifierModule.withConfig(notifierDefaultOptions),
+    NotifierModule.withConfig(notifierOptions),
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: AuthenticationInterceptor, multi: true },
