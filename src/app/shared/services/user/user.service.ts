79d9cfa4ae06fe053cc1d58b5be347176e2679b6
Mustakim-Khan <mkhan@myges.fr>
2021-07-05 18:32:31 +0200

    #18 added getAllUsers() in UserService

--- src/app/shared/services/user/user.service.ts
+++ src/app/shared/services/user/user.service.ts
@@ -2,18 +2,37 @@ import { Injectable } from '@angular/core';
 import {User} from "../../models/user/user.model";
 import {AuthenticationService} from "../authentication/authentication.service";
 import {Session} from "../../models/authentication/session.model";
+import {environment} from "../../../../environments/environment";
+import {HttpClient, HttpResponse} from "@angular/common/http";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
+  private url = environment.endpoint + '/users';
+
   constructor(
-    private authService: AuthenticationService
+    private authService: AuthenticationService,
+    private http: HttpClient
   ) { }
 
   getCurrentUser(){
     const session = this.authService.session();
     return session?.user;
+  }
+
+  /*  async getAllUsers() {
+      try {
+        const res = await this.http.get(`${ this.url }`).toPromise();
+        return res;
+      } catch {
+        return false;
+      }
+    }*/
+
+  getAllUsers(): Observable<any[]> {
+    return this.http.get<any[]>(`${ this.url }`);
   }
 }
